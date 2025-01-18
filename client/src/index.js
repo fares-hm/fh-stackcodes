ac17c84abfb5d2b09c57768ccf00832756ec1876
Fares Hmedan <fares.hmedan@gmail.com>
2025-01-18 17:39:25 +0400

    - update main

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import {thunk} from "redux-thunk";
 import { createStore, applyMiddleware, compose } from 'redux';
-import { thunk } from 'redux-thunk';
 import { reducers } from './reducers';
 import './index.css';
 import App from './App';
@@ -16,3 +16,4 @@ ReactDOM.render(
    </Provider>,
    document.getElementById('root'),
  );
+ 
