687bc54f82e75f06b6e4a4042b2b0f1d4723ed90
Fares Hmedan <fares.hmedan@gmail.com>
2025-01-18 18:33:40 +0400

    - index

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {thunk} from "redux-thunk";
+import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from 'redux';
 import { reducers } from './reducers';
 import './index.css';
